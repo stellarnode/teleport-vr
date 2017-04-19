bf89d2048098db4b02c49d2aaa93aab3bb9af619
Yaroslav Smirnov <stellarnode@ya.ru>
2017-04-19 18:46:03 +0200

    changed bg image, updated interface

--- static/js/teleport360-main.js
+++ static/js/teleport360-main.js
@@ -3,7 +3,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var heroPanoramaContainer = document.getElementById("hero-panorama");
     var heroPanorama = new PhotoSphereViewer({
-      panorama: "http://teleport-stellarnode.rhcloud.com/images/360-panorama-matador-seo.jpg",
+      // panorama: "http://teleport-stellarnode.rhcloud.com/images/360-panorama-matador-seo.jpg",
+      panorama: "http://teleport-stellarnode.rhcloud.com/images/High-quality-360-videos-and-tours-for-real-estate-2048-1024-Kitchen_Offset_2.jpg",
       container: heroPanoramaContainer
+      // default_position: {long: Math.PI/128, lat: Math.PI/64}
     });
+
+    var contactsLink = document.getElementById("contacts");
+
+    contactsLink.addEventListener("click", function() {
+
+      $("#contacts").hide();
+      $(".iwantit").hide();
+      $("#hero-welcome").append("<p class=\"hello\"><strong>Get in touch with us:</strong><br>+34 643 659 803<br>teleportvrteam@gmail.com</p>");
+
+    });
+
 });
