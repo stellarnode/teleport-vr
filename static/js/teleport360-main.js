ab7a9ff07de41b322dafff07ef53485c7d63a172
Yaroslav Smirnov <stellarnode@ya.ru>
2017-05-08 16:10:50 +0200

    changed phone number

--- static/js/teleport360-main.js
+++ static/js/teleport360-main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var heroPanoramaContainer = document.getElementById("hero-panorama");
     var heroPanorama = new PhotoSphereViewer({
       // panorama: "http://teleport-stellarnode.rhcloud.com/images/360-panorama-matador-seo.jpg",
-      panorama: "http://teleport-stellarnode.rhcloud.com/images/High-quality-360-videos-and-tours-for-real-estate-2048-1024-Kitchen_Offset_2.jpg",
+      panorama: "http://www.teleport360vr.com/images/High-quality-360-videos-and-tours-for-real-estate-2048-1024-Kitchen_Offset_2.jpg",
       container: heroPanoramaContainer
       // default_position: {long: Math.PI/128, lat: Math.PI/64}
     });
@@ -15,41 +15,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
       $("#contacts").hide();
       $(".iwantit").hide();
-      $("#hero-welcome").append("<p class=\"hello text-center\"><strong>Tell us how we can help:</strong><br>+34 643 659 803<br>teleport.vr.team@gmail.com</p>");
+      $("#hero-welcome").append("<p class=\"hello text-center\"><strong>Tell us how we can help:</strong><br>+34 684 286 121<br>teleport.vr.team@gmail.com</p>");
 
     });
 
-    $('.starter-template').forwardevents( {directEventsTo:$('#hero-panorama')} );
-
-    // function passThrough(e) {
-    //   $("#hero-panorama").each(function() {
-    //      // check if clicked point (taken from event) is inside element
-    //      var mouseX = e.pageX;
-    //      var mouseY = e.pageY;
-    //      var offset = $(this).offset();
-    //      var width = $(this).width();
-    //      var height = $(this).height();
-    //
-    //      if (mouseX > offset.left && mouseX < offset.left+width && mouseY > offset.top && mouseY < offset.top+height)
-    //        $(this).click(); // force click event
-    //   });
-    // }
-    //
-    // $(".starter-template").click(passThrough);
-    //
-    // var dthen = new Date();
-    //
-    // var doPassThrough = true;
-    // $('input').click(function(){
-    //   doPassThrough =  !doPassThrough;
-    //   if (doPassThrough){
-    //     $("#shield").click(passThrough);
-    //   } else {
-    //     $('#shield').unbind('click', passThrough);
-    //   }
-    // });
-
-
+    // $('.starter-template').forwardevents( {directEventsTo:$('#hero-panorama')} );
 
 
 });
